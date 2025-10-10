454288be04a3ea3741117b08849a778b89fde78d
lsjt5858 <15988107785@163.com>
2025-10-10 15:11:00 +0800

    .

--- Playwright_Study_week1/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# Run from script directory to make paths stable
-cd "$(dirname "$0")"
-
-pytest
-allure generate allure-results -o allure-report --clean
-allure serve ./allure-report
